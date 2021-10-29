b369ea07320c8ac01b82d9c822ed771eb5889000
Yash Mahalwal <yash@growthday.com>
2021-10-29 12:05:07 +0530

    refactor: move react logic away from redux logic

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-import { useAppSelector, useAppDispatch } from '../../app/hooks';
+import { useAppSelector, useAppDispatch } from './app/hooks';
 import {
   decrement,
   increment,
@@ -8,8 +8,8 @@ import {
   incrementAsync,
   incrementIfOdd,
   selectCount,
-} from './counterSlice';
-import styles from './Counter.module.css';
+} from './features/counter/counterSlice';
+import styles from './features/counter/Counter.module.css';
 
 export function Counter() {
   const count = useAppSelector(selectCount);
